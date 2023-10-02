da2eee070914f4d1dd34047e16f0b700d6f85b59
“vasire@mail.ru” <vasire@mail.ru>
2023-10-02 21:08:23 +0300

    ДЗ выполнено

--- reactive-service/src/main/java/ru/flamexander/reactive/service/controllers/ProductsDetailsController.java
+++ reactive-service/src/main/java/ru/flamexander/reactive/service/controllers/ProductsDetailsController.java
@@ -1,20 +1,23 @@
 package ru.flamexander.reactive.service.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import ru.flamexander.reactive.service.dtos.DetailedProductDto;
 import ru.flamexander.reactive.service.dtos.ProductDetailsDto;
+import ru.flamexander.reactive.service.entities.Product;
 import ru.flamexander.reactive.service.services.ProductDetailsService;
+import ru.flamexander.reactive.service.services.ProductsService;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api/v1/detailed")
 @RequiredArgsConstructor
 public class ProductsDetailsController {
     private final ProductDetailsService productDetailsService;
+    private final ProductsService productsService;
 
     @GetMapping("/demo")
     public Flux<ProductDetailsDto> getManySlowProducts() {
@@ -22,5 +25,38 @@ public class ProductsDetailsController {
         Mono<ProductDetailsDto> p2 = productDetailsService.getProductDetailsById(2L);
         Mono<ProductDetailsDto> p3 = productDetailsService.getProductDetailsById(3L);
         return p1.mergeWith(p2).mergeWith(p3);
+    }
+
+    // http://localhost:8189/api/v1/detailed/1
+    @GetMapping("/{id}")
+    public Mono<DetailedProductDto> getProductWithDetailsById(@PathVariable Long id) {
+        Mono<Product> product = productsService.findById(id);
+        Mono<ProductDetailsDto> productDetails = productDetailsService.getProductDetailsById(id);
+        return Mono.zip(product, productDetails)
+                .map(tuple -> new DetailedProductDto(tuple.getT1().getId(), tuple.getT1().getName(), tuple.getT2().getDescription()));
+    }
+
+    // http://localhost:8189/api/v1/detailed/list?ids=1,2,3
+    @GetMapping("/list")
+    public Flux<DetailedProductDto> getProductWithDetailsByListId(@RequestParam String ids) {
+
+        return Flux.fromIterable(Arrays.stream(ids.split(",")).toList())
+                .flatMap(idStr -> {
+                    Long id = Long.parseLong(idStr);
+                    Mono<Product> product = productsService.findById(id);
+                    Mono<ProductDetailsDto> productDetails = productDetailsService.getProductDetailsById(id);
+                    return Mono.zip(product, productDetails)
+                            .map(tuple -> new DetailedProductDto(tuple.getT1().getId(), tuple.getT1().getName(), tuple.getT2().getDescription()));
+                });
+    }
+
+    // http://localhost:8189/api/v1/detailed
+    @GetMapping()
+    public Flux<DetailedProductDto> getProductWithDetailsByListId() {
+
+        return productsService.findAll().flatMap(product -> {
+            Mono<ProductDetailsDto> productDetails = productDetailsService.getProductDetailsById(product.getId());
+            return productDetails.map(pd -> new DetailedProductDto(product.getId(), product.getName(), pd.getDescription()));
+        });
     }
 }
